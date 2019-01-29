cfb42ccb6e9fbcb9f2a02dd31348d93e4f4e69e3
kimjidms <kimjidms216@gmail.com>
2019-01-29 16:39:49 +0900

    [20190129] Add JAVA Study 05

--- JavaStudy/src/study_20190129/study_07.java
+++ JavaStudy/src/study_20190129/study_07.java
@@ -8,16 +8,15 @@ class study_07 {
 		
 		int[] a = { 3, 1, 2 };
 		
-		// a를 복사해서 { 3, 1, 2, 0, 0 } 인 배열 생성
 		int[] b = Arrays.copyOf(a, 5);
 		
-		Arrays.fill(b, 3, 5, 100); // 3 ≤ index < 5 를 100으로 채움
-		Arrays.fill(b, 10); // b의 모든 값을 10으로 채움
-		Arrays.sort(a); // 오름차순으로 정렬
+		Arrays.fill(b, 3, 5, 100);
+		Arrays.fill(b, 10);
+		Arrays.sort(a);
 		
-		String str = Arrays.toString(a); // "[1, 2, 3]" 문자열로 변경
-		boolean isSame = Arrays.equals(a, b); // 두개의 배열이 같은지 비교
-		List<String> list = Arrays.asList("abc", "def"); // 리스트로 변환
+		String str = Arrays.toString(a);
+		boolean isSame = Arrays.equals(a, b);
+		List<String> list = Arrays.asList("abc", "def");
 		
 		System.out.println(str);
 		System.out.println(isSame);
